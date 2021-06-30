de76762cc955c877600e147aa1c76e8a4f2335ae
monetas0x01 <contract@monetas.cc>
2021-06-30 17:45:37 +0800

    add static files

---
+++ release/precache-manifest.d75978f13c869edbd1c9f86be0c0c3c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb30f463a398e56c6a0eadd68d06ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "741edcc5053d59fed97a",
+    "url": "/static/css/2.6329d7b9.chunk.css"
+  },
+  {
+    "revision": "23fb6ae25394dd26dec9",
+    "url": "/static/css/main.180868fa.chunk.css"
+  },
+  {
+    "revision": "741edcc5053d59fed97a",
+    "url": "/static/js/2.1cc741df.chunk.js"
+  },
+  {
+    "revision": "bf80aaa5bacbc419a5b3260080d0d833",
+    "url": "/static/js/2.1cc741df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fb6ae25394dd26dec9",
+    "url": "/static/js/main.84c358f1.chunk.js"
+  },
+  {
+    "revision": "5fb03d75b44ff4b07242d597e1a1dc42",
+    "url": "/static/js/main.84c358f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4640ab8360b75eaaf96",
+    "url": "/static/js/runtime-main.392c243e.js"
+  },
+  {
+    "revision": "91c6bfbab94e927f41e3cb0951f709fa",
+    "url": "/static/media/sort-down.91c6bfba.svg"
+  },
+  {
+    "revision": "b97e2196fe149d375e7fe43cba740361",
+    "url": "/static/media/sort-up.b97e2196.svg"
+  },
+  {
+    "revision": "3ab13db27e6e6811aba861e884388e92",
+    "url": "/static/media/sort.3ab13db2.svg"
+  }
+]);
